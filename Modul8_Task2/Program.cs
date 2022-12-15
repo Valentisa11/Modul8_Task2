b2d54306ebde91ae0a229bed73f537e472dd85fa
Valentisa11 <valya.kobylina@yandex.ru>
2022-12-15 22:33:17 +0500

    Добавьте файлы проекта.

---
+++ Modul8_Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь:");
+        string path = Console.ReadLine();
+        DirectoryInfo di = new DirectoryInfo(path);
+       
+        if (di.Exists)
+        {
+            
+            Console.WriteLine("Размер: " + DirectoryExtension.CountSize(di) + " байт");
+        }
+        else
+        {
+            Console.WriteLine("Папка не существует по указанному пути. Проверьте правильность.");
+        }
+    }
+    internal class DirectoryExtension
+    {
+        public static long CountSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += CountSize(di);
+            }
+            return size;
+            Console.ReadKey();
+        }
+        
+    } 
+}
